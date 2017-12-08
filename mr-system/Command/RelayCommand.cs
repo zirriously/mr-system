b66babafd12fa9b652e1dc582fc4761338a9a091
MiltonNB <31879870+MiltonNB@users.noreply.github.com>
2017-12-08 11:09:24 +0100

    Command code and folder management

--- mr-system/Command/RelayCommand.cs
+++ mr-system/Command/RelayCommand.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace mr_system.Command
 {
-    class RelayCommand
+    public class RelayCommand : ICommand
     {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
 
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return ((_canExecute == null) || _canExecute());
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
