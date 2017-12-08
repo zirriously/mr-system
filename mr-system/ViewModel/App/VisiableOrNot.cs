2021575eb99e669ff0009db16976bb679889b5db
MiltonNB <31879870+MiltonNB@users.noreply.github.com>
2017-12-08 11:48:45 +0100

    .

---
+++ mr-system/ViewModel/App/VisiableOrNot.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace mr_system.ViewModel.App
+{
+    public class VisiableOrNot : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            Uri uri = new Uri(value.ToString());
+            if (uri != null)
+            {
+                if (uri.Equals("ms-appx:///View/MainPage.xaml"))
+                {
+                    return Visibility.Visible;
+                }
+            }
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
