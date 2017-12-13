50f21b05068bf7199550a275858c281f3a3e90ef
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2017-12-13 14:19:56 +0100

    added orderItemView

--- mr-system/ViewModel/Domain/Order/OrderItemViewModel.cs
+++ mr-system/ViewModel/Domain/Order/OrderItemViewModel.cs
@@ -20,19 +20,94 @@ namespace mr_system
 
         public string OrderName
         {
-           get { return _domainObject.OrderName; } 
+           get { return _domainObject.OrderName; }
+            set
+            {
+                _domainObject.OrderName = value;
+                OnPropertyChanged();
+            }
         }
 
         public string OrderInfo
         {
             get { return _domainObject.OrderInfo; }
+            set
+            {
+                _domainObject.OrderInfo = value;
+                OnPropertyChanged();
+            }
         }
 
-       
+
+        public string Measurements
+        {
+            get { return _domainObject.Measurements; }
+            set
+            {
+                _domainObject.Measurements = value;
+                OnPropertyChanged();
+            }
+        } 
+
+        
+
+        public int NumberOfItems
+        { get { return _domainObject.NumberOfItems; }
+            set
+            {
+                _domainObject.NumberOfItems = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public double Price
+        {
+            get { return _domainObject.Price; }
+            set
+            {
+                _domainObject.Price = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Materials
+        {
+            get { return _domainObject.Materials; }
+            set
+            {
+                _domainObject.Materials = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Production
+        {
+            get { return _domainObject.Production; }
+            set
+            {
+                _domainObject.Production = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string DeliveryInfo
+        {
+            get { return _domainObject.DeliveryInfo; }
+            set
+            {
+                _domainObject.DeliveryInfo = value;
+                OnPropertyChanged();
+            }
+        }
 
         public string ExpectedDelivery
         {
             get { return _domainObject.ExpectedDelivery; }
+            set
+            {
+                _domainObject.ExpectedDelivery = value;
+                OnPropertyChanged();
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
