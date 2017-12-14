696c555f1020e4fe5400f8a191bb4d72b9fad8c2
zirr <31447312+zirriously@users.noreply.github.com>
2017-12-14 16:45:29 +0100

    Mere elegant løsning til at  få maxkeycount

--- mr-system/Model/Domain/Customer/CustomerCatalog.cs
+++ mr-system/Model/Domain/Customer/CustomerCatalog.cs
@@ -9,11 +9,10 @@ namespace mr_system.Model
 {
     public class CustomerCatalog
     {
-        private static int _keyCount = 1;
+        //private static int _keyCount = 1;
         private Dictionary<int, Customer> _customers;
         private FileSource<Customer> _fileSource;
         private List<Customer> _customerList;
-        private List<int> _maxKeyInList;
 
 
         public CustomerCatalog()
@@ -21,14 +20,13 @@ namespace mr_system.Model
             _fileSource = new FileSource<Customer>(new FileStringPersistence(), new JSONConverter<Customer>());
             _customerList = new List<Customer>();
             _customers = new Dictionary<int, Customer>();
-            _maxKeyInList = new List<int>();
-            //Create(new Customer("36452658", "Albert", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
-            //Create(new Customer("36452658", "Frank", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
-            //Create(new Customer("36452658", "Mikkel", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
-            //Create(new Customer("36452658", "Per", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
-            //Create(new Customer("36452658", "Bob", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
-            //Create(new Customer("36452658", "Troels", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
-            //Create(new Customer("36452658", "Peter", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
+            Create(new Customer("36452658", "Albert", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
+            Create(new Customer("36452658", "Frank", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
+            Create(new Customer("36452658", "Mikkel", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
+            Create(new Customer("36452658", "Per", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
+            Create(new Customer("36452658", "Bob", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
+            Create(new Customer("36452658", "Troels", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
+            Create(new Customer("36452658", "Peter", "Sørensen", "Mail@mail.dk", "66254292", "Søndergade 20", "4180"));
         }
 
         public List<Customer> Customers
@@ -38,9 +36,10 @@ namespace mr_system.Model
 
         public void Create(Customer s)
         {
-            s.Key = _keyCount++;
+            s.Key = _customers.Count == 0 ? 1 : _customers.Keys.Max() + 1;
             _customers.Add(s.Key, s);
         }
+
 
         public void Delete(int key)
         {
@@ -51,13 +50,11 @@ namespace mr_system.Model
         {
             _customerList = await _fileSource.Load();
 
-            foreach (var customer in _customerList)
-            {
-                _customers.Add(customer.Key, customer);
-                _maxKeyInList.Add(customer.Key);
-            }
-
-            _keyCount = _maxKeyInList.Max() +1;
+            
+                foreach (var customer in _customerList)
+                {
+                    _customers.Add(customer.Key, customer);
+                }
         }
 
         public async void Save()
