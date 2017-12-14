758191718b06af3004a99d9b1f07f260f5ad59bf
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2017-12-14 09:30:11 +0100

    .

--- mr-system/ViewModel/Domain/Order/OrderMasterDetailsViewModel.cs
+++ mr-system/ViewModel/Domain/Order/OrderMasterDetailsViewModel.cs
@@ -1,7 +1,42 @@
-﻿namespace mr_system
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using mr_system.Model;
+
+namespace mr_system
 {
-    public class OrderMasterDetailsViewModel
+    public class OrderMasterDetailsViewModel : INotifyPropertyChanged
     {
-        
+        private OrderCatalog _catalog;
+        private OrderItemViewModel _orderItemViewModelSelected;
+
+        public OrderMasterDetailsViewModel()
+        {
+            _catalog = new OrderCatalog();
+            _orderItemViewModelSelected = null;
+        }
+
+        private List<OrderItemViewModel> CreateItemViewModelCollection(OrderCatalog catalog)
+        {
+            List<OrderItemViewModel> items = new List<OrderItemViewModel>();
+            foreach (var obj in catalog.Orders)
+            {
+                items.Add(new OrderItemViewModel(obj));
+            }
+            return items;
+        }
+
+
+        public List<OrderItemViewModel> OrderItemViewModelCollection
+        {
+            get { return CreateItemViewModelCollection(_catalog); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
