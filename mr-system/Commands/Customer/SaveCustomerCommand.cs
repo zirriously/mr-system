7beadb6400d71b9263eb908e7beb30808f96072a
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2017-12-14 13:15:14 +0100

    save command

--- mr-system/Commands/Customer/SaveCustomerCommand.cs
+++ mr-system/Commands/Customer/SaveCustomerCommand.cs
@@ -4,29 +4,26 @@ using mr_system.Model;
 
 namespace mr_system.Commands
 {
-    public class SaveCustomerCommand
+    public class SaveCustomerCommand : ICommand
     {
-        public class SaveCommand : ICommand
+        private CustomerCatalog _catalog;
+
+        public SaveCustomerCommand(CustomerCatalog catalog)
         {
-            private CustomerCatalog _catalog;
-
-            public SaveCommand(CustomerCatalog catalog)
-            {
-                _catalog = catalog;
-            }
-
-
-            public bool CanExecute(object parameter)
-            {
-                return true;
-            }
-
-            public void Execute(object parameter)
-            {
-                _catalog.Save();
-            }
-
-            public event EventHandler CanExecuteChanged;
+            _catalog = catalog;
         }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _catalog.Save();
+        }
+
+        public event EventHandler CanExecuteChanged;
     }
 }
+
--- mr-system/ViewModel/Domain/Customer/CustomerMasterDetailsViewModel.cs
+++ mr-system/ViewModel/Domain/Customer/CustomerMasterDetailsViewModel.cs
@@ -10,11 +10,13 @@ namespace mr_system
     public class CustomerMasterDetailsViewModel : INotifyPropertyChanged
     {
         private CustomerCatalog _catalog;
+
         //private CustomerDetailsViewModel _customerDetailsViewModel;
         private CustomerItemViewModel _customerItemViewModel;
 
         private DeleteCustomerCommand _deleteCommand;
         private NewCustomerCommand _newCommand;
+        private SaveCustomerCommand _saveCommand;
 
         public CustomerMasterDetailsViewModel()
         {
@@ -23,21 +25,18 @@ namespace mr_system
             //_customerDetailsViewModel = null;
             _deleteCommand = new DeleteCustomerCommand(_catalog, this);
             _newCommand = new NewCustomerCommand(_catalog, this);
+            _saveCommand = new SaveCustomerCommand(_catalog);
         }
 
-       
-        
-            private List<CustomerItemViewModel> CreateItemViewModelCollection(CustomerCatalog catalog)
+        private List<CustomerItemViewModel> CreateItemViewModelCollection(CustomerCatalog catalog)
+        {
+            List<CustomerItemViewModel> items = new List<CustomerItemViewModel>();
+            foreach (var obj in catalog.Customers)
             {
-                List<CustomerItemViewModel> items = new List<CustomerItemViewModel>();
-                foreach (var obj in catalog.Customers)
-                {
-                    items.Add(new CustomerItemViewModel(obj));
-                }
-                return items;
+                items.Add(new CustomerItemViewModel(obj));
             }
-        
-
+            return items;
+        }
 
         public List<CustomerItemViewModel> ItemViewModelCollection
         {
@@ -62,6 +61,11 @@ namespace mr_system
         public ICommand NewCommand
         {
             get { return _newCommand; }
+        }
+
+        public ICommand SaveCommand
+        {
+            get { return _saveCommand; }
         }
 
         public CustomerItemViewModel ItemViewModelSelected
